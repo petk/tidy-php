3a640b4f8e03d0c4b25773be605dcb06ee29fc93
Peter Kokot <peterkokot@gmail.com>
2018-10-12 17:17:21 +0200

    Add various enhancements

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # tidy.php in php-src
 
 [![Build Status](https://travis-ci.org/petk/tidy-php.svg?branch=php-src)](https://travis-ci.org/petk/tidy-php)
+
+This branch is made for checking how tidy.php works ni CI environment such as
+Travis.
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 git clone --depth=1 git://github.com/php/php-src
-wget https://raw.githubusercontent.com/petk/tidy-php/master/tidy.php -o php-src/scripts/dev/tidy.php
+wget https://raw.githubusercontent.com/petk/tidy-php/master/tidy.php -O php-src/scripts/dev/tidy.php
 chmod +x php-src/scripts/dev/tidy.php
