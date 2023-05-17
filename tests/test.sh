8aa3c40db57076ab06dcbeb3d67355c371c56249
Peter Kokot <peterkokot@gmail.com>
2023-05-17 21:33:34 +0200

    Fix TTY issue for GH actions

--- tests/test.sh
+++ tests/test.sh
@@ -3,6 +3,6 @@
 # Build Docker image
 docker build -t petk/tidy-tests -f ./tests/Dockerfile . || exit 1
 
-BATS="docker run -it -v `pwd`:/opt/app -w /opt/app petk/tidy-tests:latest bats"
+BATS="docker run -i -v `pwd`:/opt/app -w /opt/app petk/tidy-tests:latest bats"
 
 $BATS tests/tidy
